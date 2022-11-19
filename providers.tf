9f3fd1553dc863e9e10ddc2b9ea040f050969a40
Jinwoon gKim <97716160+jinwoongk@users.noreply.github.com>
2022-11-20 00:14:43 +0900

    add for_each

--- providers.tf
+++ providers.tf
@@ -1,16 +1,3 @@
-terraform {
-  cloud {
-    workspaces {
-      name = "t101-week5"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.22.0"
-    }
-  }
-
-  required_version = "~> 1.3.3"
+provider "aws" {
+  region = var.aws_region
 }
